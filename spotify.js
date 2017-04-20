c4d1a9672a7db338197e3bcdaa55dcbc10423e2c
htwright <harrisontwright@gmail.com>
2017-04-20 14:05:24 -0500

    next switch

--- spotify.js
+++ spotify.js
@@ -1,34 +1,25 @@
-
-
-var getFromApi = function(endpoint, query={}) {
-    const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
-    Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
-    return fetch(url).then(function(response) {
-        if (!response.ok) {
-            return Promise.reject(response.statusText);
-        }
-        return response.json();
-    });
-};
-
-
-
-var artist;
-var getArtist = function(name) {
-  
-	let query = { q: name,
-						 	limit: limit,
-						 	type: artist
-						};
-};
+//
+//
+// var getFromApi = function(endpoint, query={}) {
+//     const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+//     Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+//     return fetch(url).then(function(response) {
+//         if (!response.ok) {
+//             return Promise.reject(response.statusText);
+//         }
+//         return response.json();
+//     });
+// };
 
 
 
-const getSkrillex = `search?q=skrillex&limit=10&type=artist`;
 
-var getFromApi = function(endpoint) {
+let artist;
+
+var getFromApi = function(endpoint, query) {
 	const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
-	//Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+	Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+	console.log(url);
 	return fetch(url).then(function(response) {
 		if (!response.ok) {
 			// throw new error ('no response!');
@@ -39,31 +30,39 @@ var getFromApi = function(endpoint) {
 		if (response.artists.items.length === 0){
 			throw new Error('No results! Check query!')
 		}
-		 getArtist(response);
-		 const relatedURL = `https://api.spotify.com/v1/artists/${response.artists.items[0].id}/related-artists`;
-		 return fetch(relatedURL);
-	 }).then(stream =>{
-		 console.log(stream.body);
-		//  artist.related = stream.
+		console.log(response);
+		let resultID = response.artists.items[0].id;
+		return response;
 	 })
 
 	.catch(function(err) {
 		console.error(err);
 	})
 };
+var getArtist = function(name) {
+//https://api.spotify.com/v1/artists/{id}/related-artists
+	let query = {
+		 q: name,
+		 limit: 5,
+		 type: 'artist'
+	};
+	artist = getFromApi('search', query);
+  return getFromApi(`artists/${resultID}/related-artists`, query);
+};
 
+// var artist;
 
-
-function getArtist(name) {
-
-	artist = { artist: name.artists.items[0] };
-	// let relatedArtists = [];
-	//
-	// for(let i = 1; i < name.artists.items.length; i++) {
-	// 	relatedArtists.push(name.artists.items[i]);
-	// }
-	// artist.related = relatedArtists;
-	return artist;
-}
-
-getFromApi(getSkrillex);
+//
+// function getArtist(name) {
+//
+// 	artist = { artist: name.artists.items[0] };
+// 	let relatedArtists = [];
+//
+// 	for(let i = 1; i < name.artists.items.length; i++) {
+// 		relatedArtists.push(name.artists.items[i]);
+// 	}
+// 	artist.related = relatedArtists;
+// 	return artist;
+// }
+//
+// getFromApi(getSkrillex);
